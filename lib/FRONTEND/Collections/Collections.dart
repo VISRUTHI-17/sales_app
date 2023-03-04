3b0eed4a676b874edfa6acebc7fc6edeb7cd8e63
VISRUTHI-17 <109497004+VISRUTHI-17@users.noreply.github.com>
2023-03-04 14:53:01 +0530

    Commit 3

---
+++ lib/FRONTEND/Collections/Collections.dart
@@ -0,0 +1,206 @@
+import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:sales_app/BACKEND/detailsb.dart';
+import 'package:sales_app/FRONTEND/Collections/Collection_Details.dart';
+import 'package:sales_app/FRONTEND/Collections/NewCollection.dart';
+
+class Collections extends StatefulWidget {
+  const Collections({Key? key}) : super(key: key);
+
+  @override
+  State<Collections> createState() => _CollectionsState();
+}
+
+class _CollectionsState extends State<Collections> {
+
+  final Detailsb obj = Detailsb();
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    fetchCollectionData();
+    super.initState();
+  }
+
+  fetchCollectionData() async {
+    dynamic resultant = await obj.makeGetRequest();
+    if(resultant==null){
+      print("Unable to retrieve");
+    }else{
+      setState(() {
+        result = resultant;
+        print(result);
+      });
+    }
+  }
+  nod(String date){
+    List l = date.split('/');
+    final date2 = DateTime.now();
+    DateTime e = DateTime(int.parse(l[2]),int.parse(l[1]),int.parse(l[0]));
+    return date2.difference(e).inDays;
+  }
+  List result = [];
+  String dropdownvalue = "30";
+  List<String> items1 = ["15","30","45","60"];
+
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    var height = size.height/100;
+    var width = size.width/100;
+
+
+    return Scaffold(
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(right: 30,bottom: 30),
+        child: IconButton(
+          onPressed: (){
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                  const Newcollec()),
+            );
+          },
+          icon: const Icon(Icons.add_circle,size: 70,),
+        ),
+      ),
+      backgroundColor:  const Color(0xffA09191),
+      body: result.isEmpty?Center(
+        child: LoadingAnimationWidget.hexagonDots(color: Colors.black, size: 50),
+      ):Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 10),
+            child: Column(
+              children: [
+                Container(
+                  color: Colors.white,
+                  height: height*10,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("More than",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: width*6,
+                              fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        DropdownButton(
+                          value: dropdownvalue,
+                          // icon: const Icon(Icons.keyboard_arrow_down),
+                          items: items1.map((String item) {
+                            return DropdownMenuItem(
+                              value: item,
+                              child: Text(item,style: TextStyle(fontSize: width*4.5,fontWeight: FontWeight.bold),),
+                            );
+                          }).toList(),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              dropdownvalue = newValue!;
+                            });
+                          },
+                        ),
+                        Text("Days",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: width*6,
+                              fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        const Icon(Icons.circle,color: Colors.green,size: 30,)
+                      ],
+                    ),
+                  ),
+                ),
+                CList(context, result, const Color(0xffB6E2D3), Colors.green),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+
+  Widget CList(BuildContext context, cdata, Color color,  Color color1){
+
+    var size = MediaQuery.of(context).size;
+    var height = size.height/100;
+    var width = size.width/100;
+
+    return  SizedBox(
+      height: height*68,
+      child: ListView.builder(
+          itemCount: cdata.length,
+          itemBuilder: (BuildContext context, int index){
+            return nod(cdata[index]['orderdates'][0].toString())>=int.parse(dropdownvalue)?GestureDetector(
+              onTap: (){
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => Collection_details(details: cdata[index])));
+              },
+              child: Padding(
+                padding: const EdgeInsets.only(top: 0.5),
+                child: Container(
+                  height: height*15,
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                    color: Colors.white70,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          width: width*30,
+                          alignment: Alignment.center,
+                          child: Text(cdata[index]['companyname'].toString(),
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: width*4.5,
+                                fontWeight: FontWeight.bold
+                            ),
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.centerRight,
+                          width: width*20,
+                          child: Text(nod(cdata[index]['orderdates'][0].toString()).toString(),
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: width*4.5,
+                                fontWeight: FontWeight.bold
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: width*45,
+                          alignment: Alignment.centerRight,
+                          child: Text('Rs.${cdata[index]['openingbalance']}',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: width*4.5,
+                                fontWeight: FontWeight.bold
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ):Container();
+          }
+      ),
+    );
+  }
+
+}
---
+++ lib/FRONTEND/Collections/NewCollection.dart
@@ -0,0 +1,398 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
+
+import '../../BACKEND/detailsb.dart';
+
+
+class Newcollec extends StatefulWidget {
+  final details;
+  const Newcollec({Key? key, this.details}) : super(key: key);
+
+  @override
+  State<Newcollec> createState() => _NewcollecState();
+}
+
+class _NewcollecState extends State<Newcollec> {
+
+
+  final TextEditingController _companyname = TextEditingController();
+  final TextEditingController _amt = TextEditingController();
+  final TextEditingController _pid = TextEditingController();
+  final TextEditingController _remarks = TextEditingController();
+
+  final Detailsb obj = Detailsb();
+  String customerid = '';
+  List<String> items1 = ['Cash', 'Cheque', 'Online Payment', 'UPI'];
+  String? dropdownvalue1 = "Cash";
+
+  Validation(TextEditingController c, String v){
+    if(v=='')v='*';
+    if(c.value.text.isEmpty){
+      return v;
+    }
+    else{
+      return null;
+    }
+  }
+  bool checkall(){
+    if (_companyname.value.text.isEmpty || _pid.value.text.isEmpty || _amt.value.text.isEmpty) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    var size = MediaQuery.of(context).size;
+    var height = size.height/100;
+    var width = size.width/100;
+
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: Colors.black),
+        backgroundColor: Colors.white,
+        centerTitle: false,
+        title: Text(
+          "New Collection",
+          style: TextStyle(
+              fontWeight: FontWeight.bold, fontSize: width*4.5, color: Colors.black),
+        ),
+      ),
+      // backgroundColor: const Color(0xffA09191),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          // mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 10,left: 8, right: 8),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    height: height*6,
+                    width: width*30,
+                    child: Text(
+                      "Company Name",
+                      style: TextStyle(
+                          fontSize: width*5,
+                          fontWeight: FontWeight.bold,
+                          overflow: TextOverflow.ellipsis),
+                      maxLines: 2,
+                    ),
+                  ),
+                  Container(
+                      height: height*6,
+                      width: width*65,
+                      child: TypeAheadField<Customer?>(
+                        textFieldConfiguration: TextFieldConfiguration(
+                          decoration: InputDecoration(
+                              errorText: Validation(_companyname, 'Select the customer!'),
+                              border: const OutlineInputBorder()),
+                          controller: _companyname,
+                        ),
+                        suggestionsCallback: (pattern) async {
+                          return await obj.getcustomers(pattern);
+                        },
+                        itemBuilder: (context, Customer? suggestion) {
+                          final customer = suggestion!;
+                          return ListTile(
+                            leading: Text(customer.id),
+                            title: Text(customer.name),
+                            subtitle: Text(customer.address),
+                          );
+                        },
+                        onSuggestionSelected: (Customer? suggestion) {
+                          final customer = suggestion!;
+                          setState(() {
+                            customerid = customer.id;
+                            _companyname.text = customer.name;
+                          });
+                        },
+                      )
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    height: height*6,
+                    width: width*30,
+                    child: Text(
+                      "Amount Recieved",
+                      style: TextStyle(
+                          fontSize: width*5,
+                          fontWeight: FontWeight.bold,
+                          overflow: TextOverflow.ellipsis),
+                      maxLines: 2,
+                    ),
+                  ),
+                  SizedBox(
+                    width: width*65,
+                    height: height*6,
+                    child: TextField(
+                      controller: _amt,
+                      decoration: InputDecoration(
+                        errorText: Validation(_amt, 'Enter the amount!'),
+                        fillColor: Colors.white70,
+                        filled: true,
+                        border: const OutlineInputBorder(),
+                        labelText: 'Amount',
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    height: height*6,
+                    width: width*30,
+                    child: Text(
+                      "Payment Date",
+                      style: TextStyle(
+                          fontSize: width*5,
+                          fontWeight: FontWeight.bold,
+                          overflow: TextOverflow.ellipsis),
+                      maxLines: 2,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () => _showDatePicker(context),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(
+                              5.0,
+                              5.0,
+                            ),
+                            blurRadius: 10.0,
+                            spreadRadius: 2.0,
+                          ), //BoxShadow
+                          BoxShadow(
+                            color: Colors.white,
+                            offset: Offset(0.0, 0.0),
+                            blurRadius: 0.0,
+                            spreadRadius: 0.0,
+                          ), //BoxS
+                        ],
+                        borderRadius: BorderRadius.circular(40),
+                        color: Colors.grey,
+                      ),
+                      alignment: Alignment.center,
+                      height: height*6,
+                      width: width*50,
+                      child: _chosenDateTime == null
+                          ? Text(
+                        "Pick a Date",
+                        style: TextStyle(
+                            fontSize: width*4,
+                            fontWeight: FontWeight.bold),
+                      )
+                          : Text(
+                          _chosenDateTime.toString().substring(0, 10),
+                          style: TextStyle(
+                              fontSize: width*4,
+                              fontWeight: FontWeight.bold)),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    height: height*6,
+                    width: width*30,
+                    child: Text(
+                      "Payment Mode",
+                      style: TextStyle(
+                        fontSize: width*5,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: height*6,
+                    width:width*50,
+                    child: DropdownButton(
+                      isExpanded: true,
+                      value: dropdownvalue1,
+                      icon: const Icon(Icons.keyboard_arrow_down),
+                      items: items1.map((String item) {
+                        return DropdownMenuItem(
+                          value: item,
+                          child: Text(item),
+                        );
+                      }).toList(),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          dropdownvalue1 = newValue!;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    height: height*6,
+                    width: width*30,
+                    child: Text(
+                      "Payment ID",
+                      style: TextStyle(
+                          fontSize: width*5,
+                          fontWeight: FontWeight.bold,
+                          overflow: TextOverflow.ellipsis),
+                      maxLines: 2,
+                    ),
+                  ),
+                  SizedBox(
+                    width: width*65,
+                    height: height*6,
+                    child: TextField(
+                      controller: _pid,
+                      decoration: InputDecoration(
+                        errorText: Validation(_pid, ''),
+                        fillColor: Colors.white70,
+                        filled: true,
+                        border: OutlineInputBorder(),
+                        labelText: 'Payment ID',
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: 160,
+                child: Text(
+                  "Remarks",
+                  style: TextStyle(
+                    fontSize: width*5,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                color: Colors.grey,
+                width: width*80,
+                child: TextField(
+                  maxLines: 6,
+                  decoration: const InputDecoration(
+                    fillColor: Colors.white70,
+                    filled: true,
+                    border: OutlineInputBorder(),
+                  ),
+                  controller: _remarks,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: 160,
+                child: Text(
+                  "Proof",
+                  style: TextStyle(
+                    fontSize: width*5,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: width*65,
+                  height: height*5,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            Colors.orange
+                        ),
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)
+                            )
+                        )
+                    ),
+                    onPressed: () {
+                      if(checkall()){
+
+                      }
+                      else{
+                        print("Error");
+                      }
+
+                    },
+                    child: Text("Add collection", style: TextStyle(fontWeight: FontWeight.bold,fontSize: width*4.5),),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  DateTime? _chosenDateTime;
+  // Show the modal that contains the CupertinoDatePicker
+  void _showDatePicker(ctx) {
+    // showCupertinoModalPopup is a built-in function of the cupertino library
+    showCupertinoModalPopup(
+        context: ctx,
+        builder: (_) => Container(
+          height: 400,
+          color: const Color.fromARGB(255, 255, 255, 255),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 400,
+                child: CupertinoDatePicker(
+                    mode: CupertinoDatePickerMode.date,
+                    initialDateTime: _chosenDateTime ?? DateTime.now(),
+                    onDateTimeChanged: (val) {
+                      setState(() {
+                        _chosenDateTime = val;
+                      });
+                    }),
+              ),
+            ],
+          ),
+        ));
+  }
+}
